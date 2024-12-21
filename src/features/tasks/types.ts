d304ea67f08d95a2280099fc8ae3fea3f37b7d63
nordast <nordast@gmail.com>
2024-12-21 15:48:39 +0200

    create tasks api

---
+++ src/features/tasks/types.ts
@@ -0,0 +1,14 @@
+export enum TaskStatus {
+  BACKLOG = "BACKLOG",
+  TODO = "TODO",
+  IN_PROGRESS = "IN_PROGRESS",
+  IN_REVIEW = "IN_REVIEW",
+  DONE = "DONE",
+}
+
+// export type Task = Models.Document & {
+//   name: string;
+//   imageUrl: string;
+//   inviteCode: string;
+//   userId: string;
+// };
