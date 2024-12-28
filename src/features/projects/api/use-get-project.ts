1683a8e662812457baf476169e9078b186a0d037
nordast <nordast@gmail.com>
2024-12-28 12:55:02 +0200

    project page

---
+++ src/features/projects/api/use-get-project.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@/lib/rpc";
+
+interface UseGetProjectProps {
+  projectId: string;
+}
+
+export const useGetProject = ({ projectId }: UseGetProjectProps) => {
+  return useQuery({
+    queryKey: ["project", projectId],
+    queryFn: async () => {
+      const response = await client.api.projects[":projectId"].$get({
+        param: { projectId },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to get project");
+      }
+
+      const { data } = await response.json();
+
+      return data;
+    },
+  });
+};
