e481b576b4d3d6e4ce7f2461289dad6659bf6d2d
nordast <nordast@gmail.com>
2024-12-31 16:34:04 +0200

    oauth github

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -83,7 +83,7 @@ const app = new Hono()
 
           return {
             ...member,
-            name: user.name,
+            name: user.name || user.email,
             email: user.email,
           };
         }),
@@ -292,7 +292,7 @@ const app = new Hono()
 
     const assignee = {
       ...member,
-      name: user.name,
+      name: user.name || user.email,
       email: user.email,
     };
 
